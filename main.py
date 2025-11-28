a41d367382ea6cc26d89ee823350257329b60e58
Aayush Raj <aush110804@gmail.com>
2025-11-28 22:05:14 +0530

    added some dependices

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+item_mobile = {
+    1: {"Mobile": "Redmi", "gpu": "Snapdragon 690"},
+    2: {"Mobile": "Realme", "gpu": "Snapdragon 6 gen 4"},
+    3: {"Mobile": "Motorola", "gpu": "Snapdragon 7 gen 3"},
+    4: {"Mobile": "Iqoo", "gpu": "Snapdragon 8 gen 4"},
+}
 
 @app.get("/")
 def read_root():
@@ -10,6 +17,13 @@ def read_root():
 def about():
     return {"project": "My First API", "author": "Aayush Raj"}
 
-@app.get("/items")
-def items():
+@app.get("/items/laptop")
+def items(id):
     return {"laptop": "ColorFul","gpu": "RTX 4060"}
+@app.get("/item/mobile/{id}")
+def mobile(id: int):
+    return item_mobile.get(id,{"error": "Item not found"})
+
+@app.post("/blog")
+def create_blog():
+    return {"data": "Blog is created"}
